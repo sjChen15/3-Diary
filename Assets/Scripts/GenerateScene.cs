e25ebf94362b06fa6dca00244c9caf28ca582415
Rahma Gillan <rahmagillan2001@gmail.com>
2019-01-19 08:52:21 -0600

    Rahma's Asset Folder

---
+++ Assets/Scripts/GenerateScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class GenerateScene : MonoBehaviour
+{
+    string line;
+    public Text outputText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        try {
+            StreamReader sr = new StreamReader("TextFiles/test.txt");
+            line = sr.ReadLine();
+
+            while (line != null) {
+                //Console.WriteLine(line);
+                line = sr.ReadLine();
+                outputText.text = line;
+            
+        }
+        catch(Exception e) {}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
